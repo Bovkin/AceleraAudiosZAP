397c9709bef284f79cc3748dfedb3d69a2f54ce7
Elton Igor Cunha Pereira <34562073+Bovkin@users.noreply.github.com>
2020-10-24 18:30:34 -0300

    Adicionando nova funcionalidade

--- content.js
+++ content.js
@@ -7,30 +7,53 @@ const interval = setInterval(() => {
 
 
         const button1x = document.createElement("button");
+        const button1Halfx = document.createElement("button");
         const button2x = document.createElement("button");
         
 
         button1x.innerHTML = "1x";
         button1x.classList.add("oneTimesButton");
 
+        button1Halfx.innerHTML = "1.5x";
+        button1Halfx.classList.add("oneHalfTimesButton");
+
         button2x.innerHTML = "2x";
         button2x.classList.add("twoTimesButton");
 
+        button1x.style.color = '#66ff33';
+        
         button2x.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio");
             audios.forEach((audio) => {
                 audio.playbackRate = 2;
             })
-            //button2x.style.color = 'red';
+            button2x.style.color = '#66ff33';
+            button1Halfx.style.color = 'white';
+            button1x.style.color = 'white';
         });
+
+        button1Halfx.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio");
+            audios.forEach((audio) => {
+                audio.playbackRate = 1.5;
+            })
+            button1Halfx.style.color = '#66ff33';
+            button2x.style.color = 'white';
+            button1x.style.color = 'white';
+        });
+
         button1x.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio");
             audios.forEach((audio) => {
                 audio.playbackRate = 1;
             })
+            button1x.style.color = '#66ff33';
+            button1Halfx.style.color = 'white';
+            button2x.style.color = 'white';
         });
                 
         header.appendChild(button1x);
+        header.appendChild(button1Halfx);
         header.appendChild(button2x);
     }
 
