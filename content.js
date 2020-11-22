d72b0738da6ce6fe0a255962774ab61c9d8ee07c
Elton Igor Cunha Pereira <34562073+Bovkin@users.noreply.github.com>
2020-11-22 03:24:56 -0300

    Change version

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
-const interval = setInterval(() => {
-    const header = document.querySelector("._2O84H");
-    if(header){
-        console.log(header);
-        clearInterval(interval);
 
+const interval = setInterval(() => {
+
+    const header = document.querySelector("._2O84H");
+    if (header) {
+        clearInterval(interval);
 
         const button1x = document.createElement("button");
         const button1Halfx = document.createElement("button");
         const button2x = document.createElement("button");
-        
+        const nightMode = document.createElement("button");
 
         button1x.innerHTML = "1x";
         button1x.classList.add("oneTimesButton");
@@ -19,8 +19,12 @@ const interval = setInterval(() => {
         button2x.innerHTML = "2x";
         button2x.classList.add("twoTimesButton");
 
+        nightMode.innerHTML = "Night Mode";
+        nightMode.classList.add("nightMode");
+
         button1x.style.color = '#66ff33';
-        
+        nightMode.style.color = '#66ff33';
+
         button2x.addEventListener("click", () => {
             const audios = document.querySelectorAll("audio");
             audios.forEach((audio) => {
@@ -50,10 +54,83 @@ const interval = setInterval(() => {
             button1Halfx.style.color = '#D3D0CF';
             button2x.style.color = '#D3D0CF';
         });
-                
+
+        nightMode.addEventListener('click', function () {
+            cor = nightMode.style.color
+            if (cor == 'rgb(102, 255, 51)') {
+                nightMode.style.color = '#D3D0CF';
+
+                button2x.addEventListener("click", () => {
+                    const audios = document.querySelectorAll("audio");
+                    audios.forEach((audio) => {
+                        audio.playbackRate = 2;
+                    })
+                    button2x.style.color = 'darkgreen';
+                    button1Halfx.style.color = '#D3D0CF';
+                    button1x.style.color = '#D3D0CF';
+                });
+
+                button1Halfx.addEventListener("click", () => {
+                    const audios = document.querySelectorAll("audio");
+                    audios.forEach((audio) => {
+                        audio.playbackRate = 1.5;
+                    })
+                    button1Halfx.style.color = 'darkgreen';
+                    button2x.style.color = '#D3D0CF';
+                    button1x.style.color = '#D3D0CF';
+                });
+
+                button1x.addEventListener("click", () => {
+                    const audios = document.querySelectorAll("audio");
+                    audios.forEach((audio) => {
+                        audio.playbackRate = 1;
+                    })
+                    button1x.style.color = 'darkgreen';
+                    button1Halfx.style.color = '#D3D0CF';
+                    button2x.style.color = '#D3D0CF';
+                });
+
+            } else if (cor == 'rgb(211, 208, 207)') {
+
+                nightMode.style.color = '#66ff33';
+
+                button2x.addEventListener("click", () => {
+                    const audios = document.querySelectorAll("audio");
+                    audios.forEach((audio) => {
+                        audio.playbackRate = 2;
+                    })
+                    button2x.style.color = '#66ff33';
+                    button1Halfx.style.color = '#D3D0CF';
+                    button1x.style.color = '#D3D0CF';
+                });
+        
+                button1Halfx.addEventListener("click", () => {
+                    const audios = document.querySelectorAll("audio");
+                    audios.forEach((audio) => {
+                        audio.playbackRate = 1.5;
+                    })
+                    button1Halfx.style.color = '#66ff33';
+                    button2x.style.color = '#D3D0CF';
+                    button1x.style.color = '#D3D0CF';
+                });
+        
+                button1x.addEventListener("click", () => {
+                    const audios = document.querySelectorAll("audio");
+                    audios.forEach((audio) => {
+                        audio.playbackRate = 1;
+                    })
+                    button1x.style.color = '#66ff33';
+                    button1Halfx.style.color = '#D3D0CF';
+                    button2x.style.color = '#D3D0CF';
+                });
+            }
+        });
+
+
         header.appendChild(button1x);
         header.appendChild(button1Halfx);
         header.appendChild(button2x);
+        header.appendChild(nightMode);
     }
 
-}, 100)
+}, 1)
